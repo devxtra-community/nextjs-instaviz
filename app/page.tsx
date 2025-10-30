2a11c16a3e7943a801b4e5af36425cb18050d470
sameer4712 <sameerptmc@gmail.com>
2025-10-30 09:45:06 +0530

    login image set

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,15 @@ import { ChatBar } from '@/components/ChatBar';
 import { DashboardMain } from '@/components/DashboardMain';
 import { Navbar } from "@/components/Navbar";
 const Home: React.FC = () => {
-  const [dataUploaded, setDataUploaded] = useState<boolean>(false);
+  const [dataUploaded, setDataUploaded] = useState<boolean>(true);
 
   return (
     <>
       <div className="min-h-screen bg-gray-50 ">
         <Navbar />
         <div className="pt-13 flex flex-col md:flex-row  h-screen">
-          <DashboardMain showData={dataUploaded} />
-          <ChatBar dataUploaded={dataUploaded} setDataUploaded={setDataUploaded} />
+          <DashboardMain showData={dataUploaded}/>
+          <ChatBar dataUploaded={dataUploaded} setDataUploaded={setDataUploaded}/>
         </div>
       </div>
     </>
