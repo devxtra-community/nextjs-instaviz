e21291216e2e500858af70a9c4e6556b168aa372
sameer4712 <sameerptmc@gmail.com>
2025-11-06 14:52:55 +0530

    sample

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,7 +4,7 @@ import { ChatBar } from '@/components/ChatBar';
 import { DashboardMain } from '@/components/DashboardMain';
 import { Navbar } from "@/components/Navbar";
 const Home: React.FC = () => {
-  const [dataUploaded, setDataUploaded] = useState<boolean>(false);
+  const [dataUploaded, setDataUploaded] = useState<boolean>(true);
 
   return (
     <>
