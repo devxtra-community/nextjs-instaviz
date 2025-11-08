09783c5d6e1467d18c45c3a370a186aa1621f717
riyas <mriyastk8@gmail.com>
2025-11-08 11:06:53 +0530

    sidebar

--- components/registerusermain.tsx
+++ components/registerusermain.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import Sidebar from 
+import Sidebar from "./adminSidebar";
 import RegisteredUsersMain from "@/components/registerusermain";
 
 export default function RegisteredUsersPage() {
