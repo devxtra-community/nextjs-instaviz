ba167c196a019cb4c541c930c0e9fa47677ef7f8
Shanu V Ramesan <shanuvr112233@gmail.com>
2025-11-11 12:16:23 +0530

    axios instance

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -4,5 +4,52 @@ const axiosInstance = axios.create({
     baseURL:"http://localhost:5000",
     withCredentials:true
 })
+axiosInstance.interceptors.request.use((config)=>{
+    console.log("inside interceptor request");
+
+    const accessToken = localStorage.getItem("accessToken")
+    if(accessToken){
+        config.headers.Authorization = `bearer ${accessToken}`
+    }
+    return config
+})
+
+axiosInstance.interceptors.response.use((response)=>{
+
+    console.log("inside response ",response);
+    return response
+},
+
+    async(error)=>{
+        console.log("inside error",error);
+        const ogRequest = error.config;
+
+        if(error.response.status===401 && !ogRequest.retry){
+            ogRequest.retry=true
+            try{
+                console.log("inside try ,t");
+                const refreshToken = await axios.post("http://localhost:5000/user/newRefreshToken",
+                    {},
+                    {withCredentials:true}
+                );
+
+                const newAccessToken = refreshToken.data.newAccessToken;
+                localStorage.setItem("accessToken",newAccessToken)
+                ogRequest.headers.Authorization = `bearer ${newAccessToken}`
+                return axiosInstance(ogRequest)
+                
+            }catch(err){
+                console.log("catch in interceptor worked");
+                localStorage.clear();
+                window.location.href = "/login"
+                
+
+            }
+        }
+        return  Promise.reject(error)
+        
+    }
+    
+)
 
 export default axiosInstance;
