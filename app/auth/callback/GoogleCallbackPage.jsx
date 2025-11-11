5fe024696abfece4e61d355ba612974c9295843a
Shanu V Ramesan <shanuvr112233@gmail.com>
2025-11-11 17:12:18 +0530

    google auth

---
+++ app/auth/callback/GoogleCallbackPage.jsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function GoogleCallbackPage() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const token = searchParams.get("token");
+
+    if (token) {
+      localStorage.setItem("token", token);
+      router.push('/home');
+    } else {
+      router.push('/signup');
+    }
+  }, [router, searchParams]);
+
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <p className="text-gray-500 text-lg">Signing you in with Google...</p>
+    </div>
+  );
+}
