ff727be8bd7a47ce1faee3446b4d188f296a41ac
Shanu V Ramesan <shanuvr112233@gmail.com>
2025-11-13 09:51:52 +0530

    changed google auth env

--- components/GoogleButton.tsx
+++ components/GoogleButton.tsx
@@ -3,7 +3,7 @@ export default function GoogleButton() {
         <div>
             <button
                 type="button"
-                onClick={() => window.location.href = "http://localhost:5000/auth/google"}
+                onClick={() => window.location.href = `${process.env.NEXT_PUBLIC_API}/auth/google`}
                 className="flex w-full items-center justify-center gap-2 rounded-md border py-2.5 text-sm cursor-pointer font-medium text-gray-700 hover:bg-gray-50 transition"
 
             >
