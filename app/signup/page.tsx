b4ac40f87ef76cc2c72f845ca08dbc3741310411
Shanu V Ramesan <shanuvr112233@gmail.com>
2025-11-13 09:24:03 +0530

    changed goodle auth to .env

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -291,7 +291,7 @@ export default function SignUpPage() {
           <div className="w-full mb-4">
             <button
               type="button"
-              onClick={() => window.location.href = "http://localhost:5000/auth/google"}
+              onClick={() => window.location.href = `${process.env.NEXT_PUBLIC_API}/auth/google`}
               className="h-11 border border-gray-300 rounded-md w-full flex items-center justify-center gap-2 hover:bg-gray-100 transition"
             >
               <img src="./google.png" className="w-5 h-5 mr-1" alt="" />
