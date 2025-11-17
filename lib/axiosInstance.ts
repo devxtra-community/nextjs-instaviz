5c916e28ba2abc70c7b0ac1a45bb2bc09768a4e5
Shanu V Ramesan <shanuvr112233@gmail.com>
2025-11-17 09:56:07 +0530

    before pulling

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -1,57 +1,65 @@
 import axios from "axios";
-//interceptor need to be added
+
 const axiosInstance = axios.create({
-    baseURL:process.env.NEXT_PUBLIC_API,
-    withCredentials:true
-})
-axiosInstance.interceptors.request.use((config)=>{
-    console.log("inside interceptor request");
+  baseURL: process.env.NEXT_PUBLIC_API,
+  withCredentials: true,
+});
 
-    const accessToken = localStorage.getItem("accessToken")
-    if(accessToken){
-        console.log("inside if accesstoen");
-        
-        config.headers.Authorization = `bearer ${accessToken}`
-    }
-    return config
-})
 
-axiosInstance.interceptors.response.use((response)=>{
-
-    console.log("inside response ",response);
-    return response
-},
-
-    async(error)=>{
-        console.log("inside error",error);
-        const ogRequest = error.config;
-
-        if(error.response.status===401 && !ogRequest.retry){
-            ogRequest.retry=true
-            try{
-                console.log("inside tryt");
-                const refreshToken = await axios.post(`${process.env.NEXT_PUBLIC_API}/auth/newRefreshToken`,
-                    {},
-                    {withCredentials:true}
-                );
-
-                const newAccessToken = refreshToken.data.newAccessToken;
-                localStorage.setItem("accessToken",newAccessToken)
-                ogRequest.headers.Authorization = `bearer ${newAccessToken}`
-                return axiosInstance(ogRequest)
-                
-            }catch(err){
-                console.log("catch in interceptor worked");
-                localStorage.clear();
-                window.location.href = "/login"
-                
-
-            }
-        }
-        return  Promise.reject(error)
-        
-    }
+axiosInstance.interceptors.request.use((config) => {
+    console.log("insise axios req interceptor");
     
-)
+  const accessToken = localStorage.getItem("accessToken");
+
+  if (accessToken) {
+    console.log("user has token");
+    
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+});
+
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  
+
+  async (error) => {
+    console.log("inside axios ressponse");
+    
+    const ogRequest = error.config;
+    const accessToken = localStorage.getItem("accessToken"); 
+
+
+    if (error.response?.status === 401 && accessToken && !ogRequest.retry) {
+        console.log("inside requesting for new newaccesstoken");
+        
+      ogRequest.retry = true;
+
+      try {
+        const refreshResp = await axios.post(
+          `${process.env.NEXT_PUBLIC_API}/auth/newRefreshToken`,
+          {},
+          { withCredentials: true }
+        );
+
+        const newAccessToken = refreshResp.data.newAccessToken;
+
+        localStorage.setItem("accessToken", newAccessToken);
+        ogRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        return axiosInstance(ogRequest);
+      } catch (err) {
+        console.log("inside axios error");
+        
+        localStorage.clear();
+        window.location.href = "/login";
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
