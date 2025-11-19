4474bcfc0611045e2d462139cf3aae2df2b55ea8
sameer4712 <sameerptmc@gmail.com>
2025-11-19 10:57:05 +0530

    admin login checking

--- lib/axiosAdmin.ts
+++ lib/axiosAdmin.ts
@@ -2,27 +2,48 @@ import axios from "axios";
 
 const axiosAdmin = axios.create({
   baseURL: "http://localhost:5000",
-  withCredentials: true, 
+  withCredentials: true,
 });
 
 axiosAdmin.interceptors.request.use((config) => {
-  const token = localStorage.getItem("adminAccessToken");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+  const accessToken = localStorage.getItem("adminAccessToken");
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
   }
   return config;
 });
 
 axiosAdmin.interceptors.response.use(
+
   (response) => {
+    console.log("inside axios instance")
     return response;
   },
   async (error) => {
     console.log("ADMIN AXIOS ERROR:", error);
 
-  
-    if (error?.response?.status === 401) {
-      window.location.href = "/admin/login";
+    const originalRequest = error.config;
+
+    if (error?.response?.status === 401 && !originalRequest.retry) {
+      console.log("try for new refresh")
+      originalRequest.retry = true;
+      try {
+        const refreshToken = await axios.post(
+          `${process.env.NEXT_PUBLIC_API}/admin/refresh`,
+          {},
+          { withCredentials: true }
+        );
+        console.log("fetching new access token")
+        const newAccessToken = refreshToken.data.newAccessToken;
+        localStorage.setItem("adminAccessToken", newAccessToken);
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        return axiosAdmin(originalRequest);
+
+      } catch (err) {
+        console.log("catch in interceptor worked");
+        localStorage.clear();
+        window.location.href = "/admin/login";
+      }
     }
 
     return Promise.reject(error);
