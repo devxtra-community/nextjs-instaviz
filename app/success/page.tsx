b5a3a02821a999949fd825d891ca43b4c7536a05
sameer4712 <sameerptmc@gmail.com>
2025-11-25 16:19:08 +0530

    test

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -3,7 +3,7 @@ import { CheckCircle } from "lucide-react";
 
 export default function SuccessPage() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-[#f8f4ff] to-white px-6 text-center">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-linear-to-br from-[#f8f4ff] to-white px-6 text-center">
       <CheckCircle className="primary w-16 h-16 mb-4" />
       <h1 className="text-3xl font-bold primary mb-2">Payment Successful!</h1>
       <p className="text-gray-700 max-w-md mb-6">
--- lib/adminLogout.ts
+++ lib/adminLogout.ts
@@ -3,11 +3,8 @@ import axiosAdmin from "@/lib/axiosAdmin";
 export async function adminLogout() {
   try {
     localStorage.removeItem("adminAccessToken");
-
     document.cookie = "accessToken=; path=/; max-age=0";
-
     await axiosAdmin.post("/admin/logout");
-
     window.location.href = "/admin/login";
   } catch (err) {
     console.log("Logout error:", err);
